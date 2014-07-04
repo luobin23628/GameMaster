b8b0a7eedac1f73a24e01d9955f0909d88b631b9
binluo <binluo@sohu-inc.com>
2014-07-04 17:17:14 +0800

    first commit

---
+++ GameMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameMaster
+//
+//  Created by LuoBin on 14-7-4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
