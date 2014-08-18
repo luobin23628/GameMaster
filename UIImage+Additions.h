03febf0f22bd6a36cfa7920a66cd79412e8125b1
binluo <binluo@sohu-inc.com>
2014-08-18 16:46:00 +0800

    增加图片渲染模式

---
+++ UIImage+Additions.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Additions.h
+//  imem
+//
+//  Created by LuoBin on 14-8-18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Additions)
+
+- (UIImage *)imageWithTintColor:(UIColor *)tintColor;
+
+@end
